bd6b0278a74a4c323777d7e3390d3be2866b6b73
Patrick Smith <pgwsmith@gmail.com>
2021-09-20 09:58:54 +1000

    Can we do forms and client-side code?

--- index.js
+++ index.js
@@ -539,6 +539,53 @@ async function handleRequest(request) {
       }
       /* return new Response(result.slug, { headers: { 'content-type': contentTypes.html } }); */
     } else if (result.type === 'articleModule') {
+      if (result.slug === 'parsing') {
+        const js = `
+  //import { parse, mustEnd } from "https://unpkg.com/yieldparser@0.2.0/dist/yieldparser.umd.js?module";
+  //import { parse, mustEnd } from "https://cdn.skypack.dev/yieldparser";
+  //import { parse, mustEnd } from "https://unpkg.com/yieldparser@0.2.0/dist/yieldparser.modern.js";
+  import { parse, mustEnd } from "https://cdn.jsdelivr.net/npm/yieldparser@0.2.0/dist/yieldparser.modern.js";
+  
+  function* Digit() {
+    const [digit] = yield /^[0-9]+/;
+    const value = parseInt(digit, 10);
+    if (value < 0 || value > 255) {
+      return new Error(\`Digit must be between 0 and 255, was \${value}\`);
+    }
+    return value;
+  }
+  
+  function* IPAddress() {
+    const first = yield Digit;
+    yield '.';
+    const second = yield Digit;
+    yield '.';
+    const third = yield Digit;
+    yield '.';
+    const fourth = yield Digit;
+    yield mustEnd;
+    return [first, second, third, fourth];
+  }
+  
+  const inputEl = document.querySelector("#parsing-ip-address-input");
+  const outputEl = document.querySelector("#parsing-ip-address-output .target");
+  function apply() {
+    const input = inputEl.value;
+    console.log({ input });
+    const output = parse(input, IPAddress());
+    outputEl.textContent = JSON.stringify(output, null, 2);
+    window.Prism.highlightElement(outputEl);
+  }
+  inputEl.addEventListener('input', {
+    handleEvent(event) {
+      apply();
+    }
+  });
+  apply();
+  `;
+  return new Response(js, { headers: { 'content-type': contentTypes.javascript } });
+      }
+
       if (result.slug in pages && 'ClientModule' in pages[result.slug]) {
         return new Response(pages[result.slug].ClientModule(), { headers: { 'content-type': contentTypes.javascript } });
       } else {
